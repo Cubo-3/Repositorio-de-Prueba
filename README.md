109a7dc23d8909b633bed8b235aec2b551c7c07e
Cubo-3 <andrescastrop124@gmail.com>
2025-05-12 21:40:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Repositorio de Prueba 
+
+Este es un repositorio de prueba para practicar Git y GitHub. 
+
+saludos
+
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("¡Hola desde el repositorio de prueba!");
+
+// Función de ejemplo
+function suma(a, b) {
+    return a + b;
+}
+
+// Prueba la función
+console.log(suma(2, 3)); // Debería imprimir 5
